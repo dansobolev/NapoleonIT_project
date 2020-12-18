53404b2f8d083c788f3c0e7848411fb7e9d86025
dansobolev <daniil.sob56@gmail.com>
2020-12-18 20:03:02 +0300

    added instance from the Context Class

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from configs.config import ApplicationConfig
 from transport.sanic.configure_sanic import configure_app
 
+from context import Context
+
 
 if __name__ == '__main__':
     config = ApplicationConfig()
-    app = configure_app(config)
+    context = Context()
+    app = configure_app(config, context)
 
     app.run(
         host=config.sanic.host,
